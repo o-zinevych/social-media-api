aa82d006e0a51646fe77c78d4ab7a0d6a43a8433
Olha <olgazinevych@gmail.com>
2025-04-22 10:23:12 +0300

    add base user serializer and registration functionality

---
+++ user/serializers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "password",
+            "username",
+            "first_name",
+            "last_name",
+            "bio",
+            "image",
+        )
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 8,
+                "style": {"input_type": "password"},
+                "label": _("Password"),
+            }
+        }
+
+    def create(self, validated_data):
+        """Create a user with encrypted password"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user with encrypted password"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
