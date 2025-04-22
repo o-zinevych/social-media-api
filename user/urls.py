547514d4fa332bc16398470f304bdee852299f65
Olha <olgazinevych@gmail.com>
2025-04-22 10:40:12 +0300

    add email and password login for users to get authentication token

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import RegisterUserView
+from user.views import RegisterUserView, LoginUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
+    path("login/", LoginUserView.as_view(), name="login"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,43 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.compat import coreapi, coreschema
 from rest_framework.permissions import AllowAny
+from rest_framework.schemas import coreapi as coreapi_schema, ManualSchema
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserTokenSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+
+class LoginUserView(ObtainAuthToken):
+    serializer_class = UserTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    if coreapi_schema.is_enabled():
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="email",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Email",
+                        description="Valid email for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
