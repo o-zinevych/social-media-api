03eb05e68cc712ac582ef1f832fcee9233b6ebaf
Olha <olgazinevych@gmail.com>
2025-04-25 10:49:49 +0300

    add custom permission IsOwnerOrReadOnly for content app

---
+++ content/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Object-level permission limited to its owners to edit and delete it.
+    Other users, including unauthenticated ones, are allowed to view posts/comments.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
