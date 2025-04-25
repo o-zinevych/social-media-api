c59d917d2b3c73cf95a98b24e88f3d44cc201ecd
Olha <olgazinevych@gmail.com>
2025-04-25 11:48:05 +0300

    fix: check user is authenticated in IsOwnerOrReadOnly in has_permission

--- content/permissions.py
+++ content/permissions.py
@@ -7,6 +7,11 @@ class IsOwnerOrReadOnly(BasePermission):
     Other users, including unauthenticated ones, are allowed to view posts/comments.
     """
 
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
