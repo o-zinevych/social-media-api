603aa61f5226d0d42d3cb13e4883f1dacf36a186
Olha <olgazinevych@gmail.com>
2025-05-01 15:24:13 +0300

    set up Redis in settings.py and docker-compose, update requirements and .env.sample

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -152,6 +152,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Celery Configuration Options
+CELERY_TIMEZONE = "Europe/Kyiv"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
